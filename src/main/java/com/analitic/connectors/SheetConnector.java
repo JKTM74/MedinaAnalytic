2d335692813f1a4a249e2186eb91f96d6e7ff010
Vyacheslav <zavoznikov.vyacheslav@gmail.com>
2020-06-22 16:07:56 +0700

    Тест poi

--- src/main/java/com/analitic/connectors/SheetConnector.java
+++ src/main/java/com/analitic/connectors/SheetConnector.java
@@ -1,15 +1,19 @@
 package com.analitic.connectors;
 
 import com.analitic.services.ExcelLine;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,9 +24,9 @@ public class SheetConnector {
 
     public Map<String, Integer> getSpecialtiesFromExcel(int number) {
         Map<String, Integer> sheets = new HashMap<>();
-
-        try {
-            Workbook workbook = new XSSFWorkbook(new FileInputStream(filePath + "ВП " + number + " отделение.xlsx"));
+        File file = new File(filePath + "ВП " + number + " отделение.xlsx");
+        try (FileInputStream inputStream = new FileInputStream(file)){
+            XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
 
             for (int i = 0; i < workbook.getNumberOfSheets(); i++) {
                 sheets.put(workbook.getSheetName(i), i);
@@ -39,15 +43,33 @@ public class SheetConnector {
         try (FileInputStream inputStream = new FileInputStream(file)) {
 
             XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
-            Sheet sheet = workbook.getSheetAt(excelLine.getSheetNumber());
+            XSSFSheet sheet = workbook.getSheetAt(excelLine.getSheetNumber());
 
             int rowNumber = 4;
 
             while (sheet.getRow(rowNumber) != null) {
                 rowNumber++;
             }
-            rowNumber++;
 
+            XSSFRow row = sheet.createRow(rowNumber);
+
+            CellStyle cellStyle = workbook.createCellStyle();
+            CreationHelper createHelper = workbook.getCreationHelper();
+            cellStyle.setDataFormat(
+                    createHelper.createDataFormat().getFormat("MM.yyyy"));
+
+            cellStyle.setFillBackgroundColor(IndexedColors.AQUA.getIndex());
+            cellStyle.setFillPattern(CellStyle.BIG_SPOTS);
+
+            XSSFCell cell = row.createCell(0);
+            cell.setCellValue(new Date());
+            cell.setCellStyle(cellStyle);
+
+            inputStream.close();
+
+            FileOutputStream out = new FileOutputStream(file);
+
+            workbook.write(out);
             System.out.println(rowNumber);
         } catch (IOException e) {
             e.getMessage();
